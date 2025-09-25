d3b5ef8887f27e0cf7cd7129188420865135a18a
Christopher Woolford <clwoolford@wisc.edu>
2025-09-24 21:25:38 -0500

    code for profiling changes

--- PyPIC3D/__main__.py
+++ PyPIC3D/__main__.py
@@ -52,53 +52,66 @@ def run_PyPIC3D(config_file):
 
     ###################################################### SIMULATION LOOP #####################################
 
-    for t in tqdm(range(Nt)):
+    particles, fields = jit_loop(particles, fields, E_grid, B_grid, world, constants, curl_func, J_func, solver, bc, relativistic=relativistic)
+    # time loop to update the particles and fields
+    particles, fields = jit_loop(particles, fields, E_grid, B_grid, world, constants, curl_func, J_func, solver, bc, relativistic=relativistic)
+    # time loop to update the particles and fields
 
-        # plot the data
-        if t % plotting_parameters['plotting_interval'] == 0:
-            E, B, J, rho, *rest = fields
-            # unpack the fields
-            e_energy, b_energy, kinetic_energy = compute_energy(particles, E, B, world, constants)
-            # Compute the energy of the system
-            write_data(f"{output_dir}/data/total_energy.txt", t * dt, e_energy + b_energy + kinetic_energy)
-            write_data(f"{output_dir}/data/electric_field_energy.txt", t * dt, e_energy)
-            write_data(f"{output_dir}/data/magnetic_field_energy.txt", t * dt, b_energy)
-            write_data(f"{output_dir}/data/kinetic_energy.txt", t * dt, kinetic_energy)
-            # Write the total energy to a file
-            total_momentum = sum(particle_species.momentum() for particle_species in particles)
-            # Total momentum of the particles
-            write_data(f"{output_dir}/data/total_momentum.txt", t * dt, total_momentum)
-            # Write the total momentum to a file
-
-            for species in particles:
-                write_data(f"{output_dir}/data/{species.name}_kinetic_energy.txt", t * dt, species.kinetic_energy())
-
-
-            # write_particles_phase_space(particles, t, output_dir)
-
-            rho = compute_rho(particles, rho, world, constants)
-            # calculate the charge density based on the particle positions
-
-            mass_density = compute_mass_density(particles, rho, world)
-            # calculate the mass density based on the particle positions
-
-            fields_mag = [rho[:,:,world['Nz']//2], mass_density[:,:,world['Nz']//2]]
-            plot_field_slice_vtk(fields_mag, scalar_field_names, 2, E_grid, t, "scalar_field", output_dir, world)
-            # Plot the scalar fields in VTK format
-
-
-            vector_field_slices = [ [E[0][:,:,world['Nz']//2], E[1][:,:,world['Nz']//2], E[2][:,:,world['Nz']//2]],
-                                   [B[0][:,:,world['Nz']//2], B[1][:,:,world['Nz']//2], B[2][:,:,world['Nz']//2]],
-                                   [J[0][:,:,world['Nz']//2], J[1][:,:,world['Nz']//2], J[2][:,:,world['Nz']//2]]]
-            plot_vectorfield_slice_vtk(vector_field_slices, vector_field_names, 2, E_grid, t, 'vector_field', output_dir, world)
-            # Plot the vector fields in VTK format
-
-            if plotting_parameters['plot_vtk_particles']:
-                plot_vtk_particles(particles, t, output_dir)
-            # Plot the particles in VTK format
-
+    with jax.profiler.trace("/tmp/profile-data"):
         particles, fields = jit_loop(particles, fields, E_grid, B_grid, world, constants, curl_func, J_func, solver, bc, relativistic=relativistic)
         # time loop to update the particles and fields
+        particles, fields = jit_loop(particles, fields, E_grid, B_grid, world, constants, curl_func, J_func, solver, bc, relativistic=relativistic)
+        # time loop to update the particles and fields
+        particles, fields = jit_loop(particles, fields, E_grid, B_grid, world, constants, curl_func, J_func, solver, bc, relativistic=relativistic)
+        # time loop to update the particles and fields
+
+    # for t in tqdm(range(Nt)):
+
+    #     # plot the data
+    #     if t % plotting_parameters['plotting_interval'] == 0:
+    #         E, B, J, rho, *rest = fields
+    #         # unpack the fields
+    #         e_energy, b_energy, kinetic_energy = compute_energy(particles, E, B, world, constants)
+    #         # Compute the energy of the system
+    #         write_data(f"{output_dir}/data/total_energy.txt", t * dt, e_energy + b_energy + kinetic_energy)
+    #         write_data(f"{output_dir}/data/electric_field_energy.txt", t * dt, e_energy)
+    #         write_data(f"{output_dir}/data/magnetic_field_energy.txt", t * dt, b_energy)
+    #         write_data(f"{output_dir}/data/kinetic_energy.txt", t * dt, kinetic_energy)
+    #         # Write the total energy to a file
+    #         total_momentum = sum(particle_species.momentum() for particle_species in particles)
+    #         # Total momentum of the particles
+    #         write_data(f"{output_dir}/data/total_momentum.txt", t * dt, total_momentum)
+    #         # Write the total momentum to a file
+
+    #         for species in particles:
+    #             write_data(f"{output_dir}/data/{species.name}_kinetic_energy.txt", t * dt, species.kinetic_energy())
+
+
+    #         write_particles_phase_space(particles, t, output_dir)
+
+    #         # rho = compute_rho(particles, rho, world, constants)
+    #         # # calculate the charge density based on the particle positions
+
+    #         # mass_density = compute_mass_density(particles, rho, world)
+    #         # # calculate the mass density based on the particle positions
+
+    #         # fields_mag = [rho[:,:,world['Nz']//2], mass_density[:,:,world['Nz']//2]]
+    #         # plot_field_slice_vtk(fields_mag, scalar_field_names, 2, E_grid, t, "scalar_field", output_dir, world)
+    #         # # Plot the scalar fields in VTK format
+
+
+    #         # vector_field_slices = [ [E[0][:,:,world['Nz']//2], E[1][:,:,world['Nz']//2], E[2][:,:,world['Nz']//2]],
+    #         #                        [B[0][:,:,world['Nz']//2], B[1][:,:,world['Nz']//2], B[2][:,:,world['Nz']//2]],
+    #         #                        [J[0][:,:,world['Nz']//2], J[1][:,:,world['Nz']//2], J[2][:,:,world['Nz']//2]]]
+    #         # plot_vectorfield_slice_vtk(vector_field_slices, vector_field_names, 2, E_grid, t, 'vector_field', output_dir, world)
+    #         # # Plot the vector fields in VTK format
+
+    #         # if plotting_parameters['plot_vtk_particles']:
+    #         #     plot_vtk_particles(particles, t, output_dir)
+    #         # # Plot the particles in VTK format
+
+    #     particles, fields = jit_loop(particles, fields, E_grid, B_grid, world, constants, curl_func, J_func, solver, bc, relativistic=relativistic)
+    #     # time loop to update the particles and fields
 
     return Nt, plotting_parameters, simulation_parameters, plasma_parameters, constants, particles, fields, world
 
